f7063e0e364c9c8bd118812eee3287135d81aad5
Vinicius Santos <viniciusantos.florencio@gmail.com>
2024-04-11 21:00:53 -0300

    test buckets list

--- test/e2e/handler.test.js
+++ test/e2e/handler.test.js
@@ -1,8 +1,67 @@
-import {describe, expect, it} from '@jest/globals'
+import {describe, expect, it, beforeEach, afterEach} from '@jest/globals'
+import { CreateBucketCommand, PutObjectCommand, DeleteObjectsCommand, DeleteBucketCommand, S3Client } from "@aws-sdk/client-s3";
+import {handler} from '../../src/index'
 
-describe('Should run okay', () => {
-    it('Should pass', () => {
-        const variable = true
-        expect(variable).toBe(true)
+const setUpBucketsMock = async (s3Client, bucketName) => {
+    const command = new CreateBucketCommand({ Bucket: bucketName });
+    await s3Client.send(command)
+
+    const putObjectCommand = new PutObjectCommand({ 
+        Bucket: bucketName, 
+        Key: 'test.txt', 
+        Body: 'Hello World!',
+    });
+
+    await s3Client.send(putObjectCommand);
+    return [putObjectCommand.input]
+}
+
+const tearDownBucketsMock = async (s3Client, bucketName, Objects) => {
+    const deleteObjectsCommand = new DeleteObjectsCommand({
+        Bucket: bucketName,
+        Delete: { Objects },
+      });
+    
+    await s3Client.send(deleteObjectsCommand)
+
+    const deleteBucketCommand = new DeleteBucketCommand({
+        Bucket: bucketName,
+      });
+
+    return s3Client.send(deleteBucketCommand)
+}
+
+describe('List buckets', () => {
+    const bucketName = 'my-test-bucket'
+    const s3 = new S3Client({ 
+        region: "us-east-1", 
+        endpoint: 'http://localhost.localstack.cloud:4566', 
+        credentials: {
+            accessKeyId: 'test',
+            secretAccessKey: 'test'
+        },
+        forcePathStyle: true
+    });
+
+    let putObjectCommand
+    beforeEach(async () => { 
+        putObjectCommand = await setUpBucketsMock(s3, bucketName) 
+    })
+
+    afterEach(() => {
+        const Objects = putObjectCommand.map(({Key}) => ({Key}))
+        tearDownBucketsMock(s3, bucketName, Objects)
+    })
+
+    it('Should return the bucket\'s items list', async () => {
+        await setUpBucketsMock(s3, bucketName)
+        
+        const result = await handler()
+        const {buckets} = JSON.parse(result.body)
+
+        const name = buckets.Buckets.some(bucket => bucket.Name == bucketName)
+
+        expect(result.statusCode).toStrictEqual(200)
+        expect(name).toBe(true)
     })
 })
