6d9880994607a0f4f0f7c4aaf0d17ab8c49ad780
Vinicius Santos <viniciusantos.florencio@gmail.com>
2024-04-11 21:00:44 -0300

    list buckets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
+
 const handler = async (event) => {
+  const s3 = new S3Client({ region: "us-east-1", endpoint: 'http://localhost.localstack.cloud:4566' , credentials: {
+    accessKeyId: 'test',
+    secretAccessKey: 'test'
+  }});
+  const command = new ListBucketsCommand({})
+  const buckets = await s3.send(command);
+
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
+        buckets
       },
       null,
       2
